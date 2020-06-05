2ec33d88708bd3a7003c3fb640c5ae02cbc27462
Manjit <58348440+manjitullal@users.noreply.github.com>
2020-06-05 13:58:09 -0400

    Update commands.py

--- commands.py
+++ commands.py
@@ -10,3 +10,13 @@ os.rename(original_name, new_name)
 for i in files:
     file_name = i.replace("_(Solution)", "")
     os.rename(i,file_name)
+
+#find similarity between the strings
+#this gives the percentage of similarity between the strings, if the similarity is more than 0.5 it prints the other string
+
+from difflib import SequenceMatcher
+name = "karlanthony-towns"
+sname = ['karl-anthony-towns']
+name = [sname for sname in special_names if SequenceMatcher(None, name,sname).ratio()>0.5][0]
+>>> name
+'karl-anthony-towns'
